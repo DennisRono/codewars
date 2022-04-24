71cb97dc0fe5eb7d57270640c101fecb60294ea1
DennisRono <finnneron1@gmail.com>
2022-04-24 15:22:01 +0300

    species image stove earth tea

---
+++ js/fibonacci.js
@@ -0,0 +1,23 @@
+// I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+// I would like for you to write me a function that when given a number (n) returns the n-th number in the Fibonacci Sequence.
+
+// For example:
+
+//    nthFibo(4) == 2
+// Because 2 is the 4th number in the Fibonacci Sequence.
+
+// For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+
+
+// function nthFibo(n) {
+//     return Math.round(Math.pow((1 + Math.sqrt(5))/2, n) / Math.sqrt(5));
+// }
+
+function fib(n) {
+    let phi = (1 + Math.sqrt(5))/2;
+    let asymp = Math.pow(phi, n) / Math.sqrt(5);
+    return Math.round(asymp);
+}
+
+console.log(fib(10));
