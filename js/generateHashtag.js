9e4e08d196cf46f609a929737136ac97689e73ae
DennisRono <finnneron1@gmail.com>
2022-04-24 18:55:01 +0300

    word classroom properly pride treated

--- js/generateHashtag.js
+++ js/generateHashtag.js
@@ -16,7 +16,7 @@ const hashtag = (a) => {
     return (a.length>1&&a.length<140)?"#"+a.split(' ').map((w) => { return w[0].toUpperCase() + w.substring(1); }).join(""):false
 }
 
-console.log(hashtag("Codewars"));
+//console.log(hashtag("Codewars"));
 
 // const mySentence = "freeCodeCamp is an awesome resource";
 // const words = mySentence.split(" ");
@@ -24,24 +24,19 @@ console.log(hashtag("Codewars"));
 // let w = words.map((word) => {return word[0].toUpperCase()+word.substring(1);}).join("")
 
 // console.log(w);
-const assertEquals (a, b, c) => {
+const assertEquals = (a, b, c) => {
     (a!==b)?console.log(c):console.log("passed")
 }
 
 //hashtags tests
-if()
-describe("Tests", () => {
-    it("test", () => {
-  Test.assertEquals(hashtag(""), false, "Expected an empty string to return false")
-  Test.assertEquals(hashtag(" ".repeat(200)), false, "Still an empty string")
-  Test.assertEquals(hashtag("Do We have A Hashtag"), "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning.")
-  Test.assertEquals(hashtag("Codewars"), "#Codewars", "Should handle a single word.")
-  Test.assertEquals(hashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces.")
-  Test.assertEquals(hashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words.")
-  Test.assertEquals(hashtag("code" + " ".repeat(140) + "wars"), "#CodeWars")
-  Test.assertEquals(hashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars.")
-  Test.assertEquals(hashtag("a".repeat(139)), "#A" + "a".repeat(138), "Should work")
-  Test.assertEquals(hashtag("a".repeat(140)), false, "Too long")
-  });
-  });
+  assertEquals(hashtag(""), false, "Expected an empty string to return false")
+  assertEquals(hashtag(" ".repeat(200)), false, "Still an empty string")
+  assertEquals(hashtag("Do We have A Hashtag"), "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning.")
+  assertEquals(hashtag("Codewars"), "#Codewars", "Should handle a single word.")
+  assertEquals(hashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces.")
+  assertEquals(hashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words.")
+  assertEquals(hashtag("code" + " ".repeat(140) + "wars"), "#CodeWars")
+  assertEquals(hashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars.")
+  assertEquals(hashtag("a".repeat(139)), "#A" + "a".repeat(138), "Should work")
+  assertEquals(hashtag("a".repeat(140)), false, "Too long")
   
