f24cd82884409784289ce658225fc32ab41d8dd3
DennisRono <finnneron1@gmail.com>
2022-04-24 18:53:01 +0300

    within look farm cabin exchange

--- js/generateHashtag.js
+++ js/generateHashtag.js
@@ -24,18 +24,24 @@ console.log(hashtag("Codewars"));
 // let w = words.map((word) => {return word[0].toUpperCase()+word.substring(1);}).join("")
 
 // console.log(w);
+const assertEquals (a, b, c) => {
+    (a!==b)?console.log(c):console.log("passed")
+}
+
+//hashtags tests
+if()
 describe("Tests", () => {
     it("test", () => {
-  Test.assertEquals(generateHashtag(""), false, "Expected an empty string to return false")
-  Test.assertEquals(generateHashtag(" ".repeat(200)), false, "Still an empty string")
-  Test.assertEquals(generateHashtag("Do We have A Hashtag"), "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning.")
-  Test.assertEquals(generateHashtag("Codewars"), "#Codewars", "Should handle a single word.")
-  Test.assertEquals(generateHashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces.")
-  Test.assertEquals(generateHashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words.")
-  Test.assertEquals(generateHashtag("code" + " ".repeat(140) + "wars"), "#CodeWars")
-  Test.assertEquals(generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars.")
-  Test.assertEquals(generateHashtag("a".repeat(139)), "#A" + "a".repeat(138), "Should work")
-  Test.assertEquals(generateHashtag("a".repeat(140)), false, "Too long")
+  Test.assertEquals(hashtag(""), false, "Expected an empty string to return false")
+  Test.assertEquals(hashtag(" ".repeat(200)), false, "Still an empty string")
+  Test.assertEquals(hashtag("Do We have A Hashtag"), "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning.")
+  Test.assertEquals(hashtag("Codewars"), "#Codewars", "Should handle a single word.")
+  Test.assertEquals(hashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces.")
+  Test.assertEquals(hashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words.")
+  Test.assertEquals(hashtag("code" + " ".repeat(140) + "wars"), "#CodeWars")
+  Test.assertEquals(hashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars.")
+  Test.assertEquals(hashtag("a".repeat(139)), "#A" + "a".repeat(138), "Should work")
+  Test.assertEquals(hashtag("a".repeat(140)), false, "Too long")
   });
   });
   
